9076a52fb7309bb917885d348035f7471d5614a3
givo-matan <matanr@givoapp.com>
2018-10-01 12:01:45 +0300

    drill3 done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [`
+    .click {
+      background-color:blue;
+    }
+  `]
 })
 export class AppComponent {
+  clicks = []
+  toggleShow(){
+    this.showP = !this.showP
+    this.clicks.push(this.clicks.length)
+  }
 }
