309ab08d08a101cc639c2de457ec34447fd89afb
matan rahav <matanr@givoapp.com>
2018-10-03 10:16:25 +0300

    chapter 3 done

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model'
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  recipes:Recipe[] = [
+    new Recipe('recipe 1','a desc...','https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/recipe-image-legacy-id-1074500_11.jpg'),
+    new Recipe('recipe 1','a desc...','https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/recipe-image-legacy-id-1074500_11.jpg')
+  ]
+}
