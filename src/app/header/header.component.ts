fd671bc91036ff8b6d49a0a3c49920637a826d5f
matan rahav <matanr@givoapp.com>
2018-10-08 16:29:35 +0300

    wip routes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,9 @@
-import { Component,Output,EventEmitter } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
-  @Output() navigation = new EventEmitter<string>()
 
-  setNav(location:string){
-    console.log(location);
-    
-    this.navigation.emit(location)
-  }
 }
